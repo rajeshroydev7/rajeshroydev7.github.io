13dacb0bfcb7281e72d0bfd8051c9963790d60a3
RAJESH ROY <rajeshroya26@gmail.com>
2026-08-29 22:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Smooth scrolling for navigation links
 
+document.querySelectorAll('a[href^="#"]').forEach(function (link) {
+
+    link.addEventListener("click", function (event) {
+
+        const targetId = this.getAttribute("href");
+
+        const target = document.querySelector(targetId);
+
+        if (target) {
+
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+
+        }
+
+    });
+
+});
